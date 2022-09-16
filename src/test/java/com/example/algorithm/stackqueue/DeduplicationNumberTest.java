330eab221b309b4a401a4f6e6c5e408b6ae91841
박태건 <justerror23@gmail.com>
2022-09-16 04:36:11 +0000

    chore : Add test case

--- src/test/java/com/example/algorithm/stackqueue/DeduplicationNumberTest.java
+++ src/test/java/com/example/algorithm/stackqueue/DeduplicationNumberTest.java
@@ -9,11 +9,22 @@ class DeduplicationNumberTest {
 
     @Test
     @Tag("UNIT_TEST")
-    @DisplayName("arr = [1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1]을 return 합니다.")
+    @DisplayName("[1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.")
     void case1() {
-        int[] expect = {1, 3, 0, 1};
-        int[] duplicationNumbers = {1, 1, 3, 3, 0, 1, 1};
-        
+        int[] expect = { 1, 3, 0, 1 };
+        int[] duplicationNumbers = { 1, 1, 3, 3, 0, 1, 1 };
+
+        DeduplicationNumber deduplicationNumber = new DeduplicationNumber();
+        assertArrayEquals(expect, deduplicationNumber.solution(duplicationNumbers));
+    }
+
+    @Test
+    @Tag("UNIT_TEST")
+    @DisplayName("arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.")
+    void case2() {
+        int[] expect = { 4, 3 };
+        int[] duplicationNumbers = { 4, 4, 4, 3, 3 };
+
         DeduplicationNumber deduplicationNumber = new DeduplicationNumber();
         assertArrayEquals(expect, deduplicationNumber.solution(duplicationNumbers));
     }
