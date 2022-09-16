8205fbf0039c22910d1b42e4db3719fe39dd4081
박태건 <justerror23@gmail.com>
2022-09-16 08:45:26 +0000

    feat($Extension) : Add ExcuteTimeLoggerExtension

--- src/test/java/com/example/algorithm/stackqueue/DeduplicationNumberTest.java
+++ src/test/java/com/example/algorithm/stackqueue/DeduplicationNumberTest.java
@@ -4,12 +4,16 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import com.example.extensions.ExcutionTimeLoggerExtension;
+
+@ExtendWith(ExcutionTimeLoggerExtension.class)
 class DeduplicationNumberTest {
 
     @Test
     @Tag("UNIT_TEST")
-    @DisplayName("[1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.")
+    @DisplayName("arr = [1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.")
     void case1() {
         int[] expect = { 1, 3, 0, 1 };
         int[] duplicationNumbers = { 1, 1, 3, 3, 0, 1, 1 };
---
+++ src/test/java/com/example/extensions/ExcutionTimeLoggerExtension.java
@@ -0,0 +1,42 @@
+package com.example.extensions;
+
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
+import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+/**
+ * ExcutionTimeLoggerExtension
+ * 테스트 실행시간 로깅
+ * 
+ * @author taegeon-park23
+ */
+public class ExcutionTimeLoggerExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback {
+
+    public static enum TimeType {
+        START_TIME, END_TIME
+    }
+
+    private ExtensionContext.Store getStore(ExtensionContext context) {
+        String testClassName = context.getRequiredTestClass().getName();
+        String testMethodName = context.getRequiredTestMethod().getName();
+        return context.getStore(ExtensionContext.Namespace.create(testClassName, testMethodName));
+    }
+
+    @Override
+    public void beforeTestExecution(ExtensionContext context) throws Exception {
+        getStore(context).put(TimeType.START_TIME, System.currentTimeMillis());
+    }
+
+    @Override
+    public void afterTestExecution(ExtensionContext context) throws Exception {
+        long startTime = getStore(context).remove(TimeType.START_TIME, long.class);
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+
+        String testClassName = context.getRequiredTestClass().getName();
+        String testName = context.getDisplayName();
+
+        System.out.println(String.format("Excuted {%s} [%d]ms", testClassName + "=:" + testName, duration));
+    }
+
+}
