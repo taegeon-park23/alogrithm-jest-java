6525e391e0461f890d263c5fda46cbe71cf89262
박태건 <justerror23@gmail.com>
2022-09-17 11:27:59 +0000

    feat(level1) : 약수의 합

---
+++ src/main/java/com/example/algorithm/level1/FactorAdder.java
@@ -0,0 +1,18 @@
+package com.example.algorithm.level1;
+
+/**
+ * FactorAddor 약수의 합
+ * 
+ * 정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성
+ * 
+ * @see https://school.programmers.co.kr/learn/courses/30/lessons/12928
+ */
+public interface FactorAdder {
+    /**
+     * 정수를 입력받아 n의 약수를 모두 더한 값을 리턴
+     * 
+     * @param number 정수, 0 이상 3000이하인 정수
+     * @return 약수의 합
+     */
+    public int solution(int number);
+}
---
+++ src/test/java/com/example/algorithm/level1/FactorAdderTest.java
@@ -0,0 +1,39 @@
+package com.example.algorithm.level1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import com.example.extensions.ExcutionTimeLoggerExtension;
+
+@ExtendWith(ExcutionTimeLoggerExtension.class)
+class FactorAdderTest {
+
+    @Test
+    @DisplayName("12의 약수는 1, 2, 3, 4, 6, 12입니다. 이를 모두 더하면 28입니다.")
+    void case1() {
+        // Given
+        FactorAdder factorAdder = new FactorAdderImpl();
+
+        // When
+        int number = 12;
+
+        // Then
+        assertEquals(28, factorAdder.solution(number));
+    }
+
+    @Test
+    @DisplayName("5의 약수는 1, 5입니다. 이를 모두 더하면 6입니다.")
+    void case2() {
+        // Given
+        FactorAdder factorAdder = new FactorAdderImpl();
+
+        // When
+        int number = 5;
+
+        // Then
+        assertEquals(6, factorAdder.solution(number));
+    }
+}
