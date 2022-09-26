043a31cf99627ffa725a6aed13bfc8fe35f39709
박태건 <justerror23@gmail.com>
2022-09-26 11:15:10 +0000

    feat() : com.example.algorithm.level1.ConvertStringInteger

---
+++ src/main/java/com/example/algorithm/level1/ConvertStringInteger.java
@@ -0,0 +1,18 @@
+package com.example.algorithm.level1;
+
+/**
+ * ConvertStringInteger 문자열을 정수로 바꾸기
+ * 문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
+ * 
+ * @see https://school.programmers.co.kr/learn/courses/30/lessons/12944
+ */
+public interface ConvertStringInteger {
+
+    /**
+     * 
+     * @param s s의 길이는 1 이상 5이하, s의 맨앞에는 부호(+, -)가 올 수 있습니다 .s는 "0"으로 시작하지 않습니다. s는
+     *          부호와 숫자로만 이루어져있습니다.
+     * @return
+     */
+    public int solution(String s);
+}
